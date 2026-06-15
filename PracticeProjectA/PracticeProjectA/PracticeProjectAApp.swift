7dd3e1a2e36b36770e029acafe7241696f273010
kristopher-coder <kristopher.ross@hgs.hiddengeniusproject.org>
2026-06-15 14:30:20 -0700

    upload PracticeProblemA
    -

---
+++ PracticeProjectA/PracticeProjectA/PracticeProjectAApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeProjectAApp.swift
+//  PracticeProjectA
+//
+//  Created by KRISTOPHER ROSS on 6/15/26.
+//
+
+import SwiftUI
+
+@main
+struct PracticeProjectAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
